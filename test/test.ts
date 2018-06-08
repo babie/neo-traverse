960f7affd69fc12f507f8269f21d4f5ac588885e
babie <babie.wildheart@gmail.com>
2018-06-09 03:32:01 +0900

    test: Move target vars from `it` to `describe`

--- test/test.ts
+++ test/test.ts
@@ -4,14 +4,15 @@ const assert = powerAssert
 import { traverse } from '../'
 
 describe('traverse()', () => {
-  it('get() return any', () => {
-    const bar: symbol = Symbol('bar')
-    const obj = { foo: [{}, { [bar]: 'baz' }] }
+  const bar: symbol = Symbol('bar')
+  const obj = { foo: [{}, { [bar]: 'baz' }] }
+  const tr = traverse(obj)
 
-    assert(traverse(obj).get('foo') === obj.foo)
-    assert(traverse(obj).get('foo', 0) === obj.foo[0])
-    assert(traverse(obj).get('foo', 1) === obj.foo[1])
-    assert(traverse(obj).get('foo', 1, bar) === obj.foo[1][bar])
-    assert(traverse(obj).get('foo', 1, 'qux') === undefined)
+  it('get() return any', () => {
+    assert(tr.get('foo') === obj.foo)
+    assert(tr.get('foo', 0) === obj.foo[0])
+    assert(tr.get('foo', 1) === obj.foo[1])
+    assert(tr.get('foo', 1, bar) === obj.foo[1][bar])
+    assert(tr.get('foo', 1, 'qux') === undefined)
   })
 })
