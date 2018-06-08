e94b40cd842008a8dd9bbecd8da1163ba6adc54a
babie <babie.wildheart@gmail.com>
2018-06-09 03:01:46 +0900

    test: Add describe

--- test/test.ts
+++ test/test.ts
@@ -3,10 +3,12 @@ const assert = powerAssert
 
 import { traverse } from '../'
 
-it('get value', () => {
-  const bar: symbol = Symbol('bar')
-  const obj = { foo: [{}, { [bar]: 'baz' }] }
+describe('traverse()', () => {
+  it('get() return value', () => {
+    const bar: symbol = Symbol('bar')
+    const obj = { foo: [{}, { [bar]: 'baz' }] }
 
-  assert(traverse(obj).get('foo', 0, bar) === undefined)
-  assert(traverse(obj).get('foo', 1, bar) === 'baz')
+    assert(traverse(obj).get('foo', 0, bar) === undefined)
+    assert(traverse(obj).get('foo', 1, bar) === 'baz')
+  })
 })
