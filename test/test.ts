e2e167b8c12ae821bea0d0710e99f51f1b0d5faa
babie <babie.wildheart@gmail.com>
2018-06-09 03:20:58 +0900

    test: Add more test cases

--- test/test.ts
+++ test/test.ts
@@ -4,11 +4,14 @@ const assert = powerAssert
 import { traverse } from '../'
 
 describe('traverse()', () => {
-  it('get() return value', () => {
+  it('get() return any', () => {
     const bar: symbol = Symbol('bar')
     const obj = { foo: [{}, { [bar]: 'baz' }] }
 
-    assert(traverse(obj).get('foo', 0, bar) === undefined)
-    assert(traverse(obj).get('foo', 1, bar) === 'baz')
+    assert(traverse(obj).get('foo') === obj.foo)
+    assert(traverse(obj).get('foo', 0) === obj.foo[0])
+    assert(traverse(obj).get('foo', 1) === obj.foo[1])
+    assert(traverse(obj).get('foo', 1, bar) === obj.foo[1][bar])
+    assert(traverse(obj).get('foo', 1, 'qux') === undefined)
   })
 })
